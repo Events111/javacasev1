b1a270c1da0aaf35cde316373e13b892dac84ef7
书生论剑 <40242289+Events111@users.noreply.github.com>
2018-06-27 21:29:47 +0800

    Initial commit

---
+++ src/main/java/events/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package events;
+
+/**
+ * @author iEvents
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("aaaaa bbb ccc");
+
+	}
+
+}
